ecf1ae25953234326f30feb6ead941031b444cee
Nikola Mirchev <nicola.mirchev@gmail.com>
2022-04-23 12:36:22 +0300

    Change font folor in some fileds and made mobile nav closes automatically

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ const root = document.querySelector('main');
 
 page(decorateContext);
 page('/', teamPage);
-page('/RouteSettIt', teamPage);
-page('/index.html', teamPage);
 page('/team', teamPage);
 page('/media', mediaPage);
 page('/services', servicesPage);
@@ -29,23 +27,3 @@ function decorateContext(ctx, next){
   next();
 }
 
-// const teamParticipants = document.querySelector('.w3-row');
-// teamParticipants.childNodes.forEach(e => e.addEventListener('click', learnMore));
-
-
-
-// function learnMore(event){
-
-//     if(event.target.tagName =='A'){
-//         console.log(event.target);
-//         const parentSection = event.target.parentNode;
-//         const infoSec = Array.from(parentSection.childNodes).find(n => n.className == 'Setter-more-info');
-//         infoSec.style.display = 'block';
-//         infoSec.children[0].style.display = 'block';
-//         event.target.style.display = 'none';
-
-//         infoSec.childNodes[0].addEventListener('click', (ev) => console.log(ev));
-        
-//     }
-
-// }
--- src/views/detailsPage.js
+++ src/views/detailsPage.js
@@ -10,7 +10,7 @@ import {members} from '../../templates/members.js';
 const detailsSectionTemplate = (member) => html`
     <div class="backGround " id="team">
          <div class="w3-container w3-padding-64 w3-center centeredContent symertry"  >
-           <h2>${member.name}</h2>
+           <h2 class="names">${member.name}</h2>
             <div class="setter-settingExperience"> 
              <div> ${member.details} </div>
              <div> <img src="${member.img2}" alt="${member.name}"> </div>
