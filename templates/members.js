5255080ae66d4ad80b1b9625bfec40184e464b98
Nikola Mirchev <nicola.mirchev@gmail.com>
2022-05-07 18:45:52 +0300

    Removed Emiliyan from the crew :(

--- templates/members.js
+++ templates/members.js
@@ -1,14 +1,6 @@
-export const members = [{
-                        name : 'Emiliyan Kolevski',
-                        img : '../src/media/Emo-1.jpg',
-                        img2 : '../src/media/Emo-2.jpg',
-                        img3 : '../src/media/Emo-3.jpg',
-                        hardestBoulder : '8b',
-                        harderstRoute : '8c+',
-                        details : 'Emiliyan Kolevski is definitely one of the best and best known climbers in Bulgaria and also other countries. He is 100% climbing dedicated from more than 20 years and has climbed with the best in the world and has participated in really attractive events. He is famous for his rock projects, but his appearance in the gym is on a really high level.',
-                        details2 : 'Emiliyan, a.k.a Emo, is currently working as a wall dealer in Walltopia and part-time routesetter for their projects and in their local gym in Sofia. He has participated in the organization of many events and is setting for competition abroad. Every year he attends many climbing related events like walls and new holds exhibition. Emo is the guy who knows what is new and cool in the sphere and introduces it to his mates.',
-                        id : '1'                     
-                },
+export const members = [
+                    
+                ,
                         {name : 'Martin Penkov',
                         img : '../src/media/Marto-1.jpg',
                         img2 : '../src/media/Marto-2.jpg',
@@ -17,7 +9,7 @@ export const members = [{
                         harderstRoute : '8b+',
                         details : 'Martin Penkov has been in the sphere for more than 15 years. Starts climbing when he is 16 years old and progresses quickly.  He studies in the National Sport University and wins many of the national competitions and takes part in some European and world competitions in Europe. His main job is also related to the sport - he repairs climbing shoes.',
                         details2 : 'When Martin stops competitions because of the lack of conditions in Bulgaria, he wants to contribute to the sport and youth climbers. He is trainer in one of the best climbing teams in the country and participate in the organization and routessetting for many competitions for children and also for adults. Marto also loves exploring and creating new routes on the rocks which gives him the incredibly ability to implement different moves on the wall. ',
-                        id : '2'
+                        id : '1'
                  },
                  {      
                         name : 'Nikola Mirchev',
@@ -28,7 +20,7 @@ export const members = [{
                         harderstRoute : '8a',
                         details : 'Nikola is a young, motivated member of our crew with much experience for his age in the climbing field. In his youth he compete actively, but when he starts routesetting, it takes his attention and passion. He works in the biggest climbing gym in Bulgaria for 2 years like a routesetter and 1 year as a climbing trainer. Participates in the routesetting organization and the distribution of work in the gym.',
                         details2 : 'He is really motivated about the sport and wants to contribute and expand the routesetting job. The main idea of “RoutesetIt” is his and gives him hope that this mastery could be populated and seen as a separate occupation. He has participated in the organization of climbing events where he took many initiatives to make the attendance and experience of the participants unique. He believes that with the development of the sport there are many gyms which can benefit from our motivation and experience which will benefit a large audience that appreciate well done job and well organized events.',
-                        id : '3'
+                        id : '2'
                 }, 
                 {      
                         name : 'Dimitar Rusev',
@@ -39,7 +31,7 @@ export const members = [{
                         harderstRoute : '8c',
                         details : 'Dimitar, a.k.a Mitaka is the youngest, but definitely not the weakest member of our group. He is only 18 years, but his creativity and intelligence will make you feel younger in the situation. He is still in the competitions and heavy trainings which is great opportunity for our team when he is back from another europe championship and tells us about everything new and how current competitors perceive the climbing and styles of the routes.',
                         details2 : 'Mitaka works diligently and always aims for the best of the situation. His perfectionism is sometimes annoying for his mates, but definitely gives the best results. Mitaka has already participated in setting for gyms and often he has to do it for himself and better prepare for the competitions. His motivation for live and sports discipline is what makes the workflow pleasant and  optimal.',
-                        id : '4'
+                        id : '3'
                 },
                 {
                         name : 'Matey Micev',
@@ -50,7 +42,7 @@ export const members = [{
                         harderstRoute : '8b',
                         details : 'Matey is another young and an active competitor member of our team. His motivation about climbing is on another level. Everything in his daily routine is related to this kind of movement and everything that he does is to improve his skills, cooperating abilities and to contribute to the community. Motivation and dedication is what makes the professionalists good at their jobs and Matey is a 100% of both.',
                         details2 : 'He is currently in the national team and often travels in the Europe\'s best gyms which is a great opportunity for our team, because his observations keep us up to date to the last detail. He also actively climbs on rocks. Competitor who enjoys rocks is the best kind of routesetter, because practically all sorts of movements that can be done from human physics pass through this routesetter on a daily basis. We are so glad to have such a member in our team.' ,
-                        id : '5'
+                        id : '4'
                 }                     
                     ];
 
