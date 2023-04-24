3a7b0884a81e8487b8f1260403471c549e0a6d72
Nikola Mirchev <nicola.mirchev@gmail.com>
2023-04-24 18:42:20 +0300

    Added more media

--- src/views/detailsPage.js
+++ src/views/detailsPage.js
@@ -1,36 +1,26 @@
 import { html } from "../lib.js";
-import {members} from '../../templates/members.js';
-
-
-
-
-
-
+import { members } from "../../templates/members.js";
 
 const detailsSectionTemplate = (member) => html`
-    <div class="backGround " id="team">
-         <div class="w3-container w3-padding-64 w3-center centeredContent symertry"  >
-           <h2 class="names">${member.name}</h2>
-            <div class="setter-settingExperience"> 
-             <div> ${member.details} </div>
-             <div> <img src="${member.img2}" alt="${member.name}"> </div>
-            </div>
-            <br>
-         
-            <div class="setter-settingExperience" > 
-                <div> <img src="${member.img3}" alt="${member.name}"> </div>
-                <div> ${member.details2} </div>
-            </div>
+  <div class="backGround " id="team">
+    <div class="w3-container w3-padding-64 w3-center centeredContent symertry">
+      <h2 class="names">${member.name}</h2>
+      <div class="setter-settingExperience">
+        <div>${member.details}</div>
+        <div><img src="${member.img2}" alt="${member.name}" /></div>
+      </div>
+      <br />
 
-         </div>
+      <div class="setter-settingExperience">
+        <div><img src="${member.img3}" alt="${member.name}" /></div>
+        <div>${member.details2}</div>
+      </div>
     </div>
+  </div>
+`;
 
-` 
+export function detailsPage(ctx) {
+  const member = members.filter((member) => member.id == ctx.params["id"]);
 
-export function detailsPage(ctx){
-
-    const member = members.filter(member => member.id == ctx.params['id']);
-
-    ctx.render(detailsSectionTemplate(member[0]));
-
+  ctx.render(detailsSectionTemplate(member[0]));
 }
--- templates/mediaPhotos.js
+++ templates/mediaPhotos.js
@@ -1,10 +1,22 @@
 export const photos = [
-    { photo : '/src/media/Durres.png',
-      text : 'Eco Park Durres - 90 routes'  
-    },
-    {
-        photo : '/src/media/Europe-center.jpg',
-        text : 'Sport center "Europe" - Sofia - 50 routes'
-    }
-
-    , ]
+  {
+    photo: "/src/media/Europe-center.jpg",
+    text: 'Sport center "Europe", Bulgaria - 50 routes (From 4a to 7c)',
+    site: "https://climbing.sportcentereurope.bg/",
+  },
+  {
+    photo: "/src/media/LevelUp.jpg",
+    text: "Level up Boulder Gym, Belgum - 80 boulders (From 4a to 7c)",
+    site: "https://www.levelup.be/",
+  },
+  {
+    photo: "/src/media/TrainSF.jpg",
+    text: "Train SF gym, UAE - 50 boulders (From 4a to 7b)",
+    site: "https://www.train-sf.com/",
+  },
+  {
+    photo: "/src/media/Durres.png",
+    text: "Eco Park Durres, Albania - 90 routes (From 4a to 8a)",
+    site: "",
+  },
+];
